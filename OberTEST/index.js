930acdbc6a3b8a0edfbd62f64f551fe69b8c5085
JHKkmu <jhk8715@kmu.kr>
2022-05-19 15:27:29 +0900

    1

--- OberTEST/index.js
+++ OberTEST/index.js
@@ -1,3 +1,8 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
 const router = require('./router/index')
+var session = require('express-session')
 
 app.use(router)
