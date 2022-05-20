c053a3c46755c953f1c231a3995863fae656dbb7
JHKkmu <jhk8715@kmu.kr>
2022-05-20 21:33:32 +0900

    TEST Page

--- OberTEST/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const mysql = require('mysql')
-const bodyParser = require('body-parser')
-const router = require('./router/index')
-var session = require('express-session')
-
-app.use(router)
---
+++ OberTEST/routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const models = require('../models');
+const router = express.Router();
+
+router.get('/board', function(req, res, next) {
+    res.render('show');
+});
+
+router.post('/board', function(req, res, next) {
+    let body = req.body;
+
+    models.post.create({
+        title: body.inputTitle,
+        writer: body.inputWriter
+    })
+        .then( result => {
+            console.log("데이터 추가 완료");
+            res.redirect("/board");
+        })
+        .catch( err => {
+            console.log("데이터 추가 실패");
+        })
+});
+
+module.exports = router;
